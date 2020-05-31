e47bfdbb52dae3843a03a6e8ac9552da45d137e3
Yuan Ren <reyren179@gmail.com>
2020-05-31 17:59:24 +0800

    using string to list in exec command
    add color ouput msg
    
    Signed-off-by: Yuan Ren <reyren179@gmail.com>

--- generate_gpu_rs.py
+++ generate_gpu_rs.py
@@ -6,6 +6,19 @@ import logging
 import os
 import sys
 
+def print_msg(svcName, port, svcSelector, podName, containerName, mount_path, volume_name, claim_name):
+    print("\033[0;32m====> svc.metadata.name: %s\033[0m"%(svcName))
+    print("\033[0;32m====> svc.metadata.namespace: %s\033[0m"%(namespace))
+    print("\033[0;32m====> svc.spec.ports.port: %s\033[0m"%(port))
+    print("\033[0;32m====> svc.spec.selector.run: %s\033[0m"%(svcSelector))
+    print("\033[0;32m====> pod.metadata.name: %s\033[0m"%(podName))
+    print("\033[0;32m====> pod.spec.containers.image: %s\033[0m"%(image_name))
+    print("\033[0;32m====> pod.spec.containers.name: %s\033[0m"%(containerName))
+    print("\033[0;32m====> pod.spec.containers.volumeMounts.name: %s\033[0m"%(volume_name))
+    print("\033[0;32m====> pod.spec.containers.volumeMounts.mountPath: %s\033[0m"%(mount_path))
+    print("\033[0;32m====> pod.spec.volumes.name: %s\033[0m"%(volume_name))
+    print("\033[0;32m====> pod.spec.volumes.persistentVolumeClaim.persistentVolumeClaim: %s\033[0m"%(claim_name))
+
 def generate_service_template(serviceName, selectorName):
 
     py_object = {
@@ -22,6 +35,9 @@ def generate_gpu_pod_slave(yaml_file, num):
     # generate service
     svcName = ''.join(['pod-svc-slave', str(num)])
     svcSelector = ''.join(['pod-selector-slave', str(num)])
+    mount_path = "/usr/share/horovod"
+    volume_name = "volume-name"
+    claim_name = "pod-pvc-volume-1"
     py_object_svc = generate_service_template(svcName, svcSelector)
 
     # generate pod
@@ -31,13 +47,14 @@ def generate_gpu_pod_slave(yaml_file, num):
                 'apiVersion':'v1',
                 'kind':'Pod',
                 'metadata':{'name':podName,'namespace':namespace,'labels':{'run':svcSelector}},
-                'spec':{'containers':[{'name':containerName,'image':image_name,'command':commands,'args':arg,'resources':{'limits':{'nvidia.com/gpu':1}}}]}
-                }
+                'spec':{'containers':[{'name':containerName,'image':image_name,'command':['/bin/sh','-c'],'args':list_arg,'resources':{'limits':{'nvidia.com/gpu':1}},'volumeMounts':[{'name':volume_name,'mountPath':mount_path}]}],'volumes':[{'name':volume_name,'persistentVolumeClaim':{'claimName':claim_name}}],"nodeSelector":{'disktype':'gpu-node1'}}}
     
     # join
     file = open(yaml_file, 'w')
     yaml.dump_all([py_object_svc, py_object], file)
     file.close()
+
+    print_msg(svcName, 22, svcSelector, podName, containerName, mount_path, volume_name, claim_name)
 
 
 if __name__ == '__main__':
@@ -47,8 +64,9 @@ if __name__ == '__main__':
 
         # TODO:some paramaters can be modified
         image_name = "horovod/horovod:0.18.1-tf1.14.0-torch1.2.0-mxnet1.5.0-py3.6"
-        commands = "[ \"/bin/sh\", \"-c\" ]"
-        arg = "[ \"cd /usr/share/horovod/SSD-Tensorflow/;apt update -y;apt install ssh vim sshpass -y;echo root:admin123|chpasswd;tmp='PermitRootLogin yes';sed -i \\\"/^#PermitRootLogin/c$tmp\\\" /etc/ssh/sshd_config;/etc/init.d/ssh restart; tail -f /dev/null \"]"
+        arg = 'cd /usr/share/horovod/SSD-Tensorflow/;apt update -y;apt install ssh vim sshpass -y;echo root:admin123|chpasswd;tmp=\"PermitRootLogin yes\";sed -i \"/^#PermitRootLogin/c$tmp\" /etc/ssh/sshd_config;/etc/init.d/ssh restart; tail -f /dev/null '
+        list_arg = []
+        list_arg.append(arg)
         current_path = os.path.abspath(".")
 
         if gpu_num > 1:
@@ -56,6 +74,7 @@ if __name__ == '__main__':
                 file_name = ''.join(['gpu-pod-slave', str(i), '.yaml'])
                 yaml_path = os.path.join(current_path, file_name)
                 generate_gpu_pod_slave(yaml_path, i)
+                #print("\033[4m%s created\033[0m"%(file_name))
                 print("%s created"%(file_name))
 
         yaml_path = os.path.join(current_path, "gpu-job-master.yaml")

ea24c4c9955351a1a26754bc5f67c88c1bb627f2
Yuan Ren <reyren179@gmail.com>
2020-06-05 11:07:59 +0800

    Add tensorflow to be an another frame
    generate corresponding worker files
    
    Signed-off-by: Yuan Ren <reyren179@gmail.com>

---
+++ generate_tensorflow_rs.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python2
+#-*- coding: UTF-8 -*-
+
+import yaml
+import logging
+import os
+import sys
+
+def get_nodes_name():
+    
+    cpu_nodes = ""
+    gpu_nodes = ""
+    # cpu
+    for i in range(1, int(cpu_num) + 1):
+        cpu_svcName = ''.join(['pod-svc-ps', str(i)])
+        cpu_nodes += cpu_svcName + "." + namespace + ".svc.cluster.local:2222"
+        if not i == int(cpu_num):
+            cpu_nodes += ","
+    # gpu
+    for i in range(1, int(gpu_num) + 1):
+        gpu_svcName = ''.join(['pod-svc-worker', str(i)])
+        gpu_nodes += gpu_svcName + "." + namespace + ".svc.cluster.local:2222"
+        if not i == int(gpu_num):
+            gpu_nodes += ","
+    
+    return cpu_nodes,gpu_nodes
+
+def generate_service_template(serviceName, selectorName):
+
+    py_object = {
+                'apiVersion':'v1',
+                'kind':'Service',
+                'metadata':{'name':serviceName,'namespace':namespace},
+                'spec':{'selector':{'run':selectorName},'ports':[{'name':'ssh','protocol':'TCP','port':22,'targetPort':22},{'name':'tf','protocol':'TCP','port':2222,'target':2222}]}
+                }
+
+    return py_object
+
+def generate_gpu_pod_worker(num, yaml_file):
+
+    list_arg_pod = []
+    
+    # generate svc
+    svcName = ''.join(['pod-svc-worker', str(num)])
+    svcSelector = ''.join(['pod-selector-worker', str(num)])
+    # generate svc
+    py_object_svc = generate_service_template(svcName, svcSelector)
+
+    #generate pod
+    arg_pod = arg_base
+    arg_pod += ' --jobname="worker" --taskindex=' + str(num - 1) + ";tail -f /dev/null;"
+    list_arg_pod.append(arg_pod)
+    podName = ''.join(['pod-worker', str(num)])
+    containerName = ''.join(['pod-container-worker', str(num)])
+    mount_path = "/usr/share/horovod"
+    volume_name = "volume-name"
+    claim_name = "pod-pvc-volume-1"
+    py_object = {
+                'apiVersion':'v1',
+                'kind':'Pod',
+                'metadata':{'name':podName,'namespace':namespace,'labels':{'run':svcSelector}},
+                'spec':{'containers':[{'name':containerName,'image':image_name,'command':['/bin/sh','-c'],'args':list_arg_pod,'resources':{'limits':{'nvidia.com/gpu':1}},'volumeMounts':[{'name':volume_name,'mountPath':mount_path}]}],'volumes':[{'name':volume_name,'persistentVolumeClaim':{'claimName':claim_name}}]}}
+
+    file = open(yaml_file, 'w')
+    yaml.dump_all([py_object_svc, py_object], file)
+    file.close()
+
+
+if __name__ == '__main__':
+    try:
+        namespace = sys.argv[1]
+        gpu_num = sys.argv[2] # this num stand by the number of GPU pods
+        cpu_num = sys.argv[3] # this num stand by the number of CPU pods
+        exec_true = sys.argv[4]
+
+        # TODO:some paramaters can be modified
+        image_name = 'okwrtdsh/anaconda3:tf-10.0-cudnn7'
+        # base command
+        arg_base = 'cd /usr/share/horovod/tensorflow_distributed/; python example.py '
+        current_path = os.path.abspath(".")
+
+        # 提前生成CPU:2222,CPU:2222 GPU:2222,GPU:2222
+        # 得先确定svcname
+
+        cpu_nodes_str, gpu_nodes_str = get_nodes_name()
+        arg_base += cpu_nodes_str + " " + gpu_nodes_str
+
+        for i in range(1, int(gpu_num) + 1):
+            file_name = ''.join(['gpu-pod-worker', str(i), '.yaml'])
+            yaml_path = os.path.join(current_path, file_name)
+            generate_gpu_pod_worker(i, yaml_path)
+
+        #generate_cpu_pod_ps(cpu_num)
+
+
+    except IOError as e:
+        logging.error("Failed to create RS in %s namespaces: {}" %(namespace))
--- main.sh
+++ main.sh
@@ -27,13 +27,14 @@ DISTRIBUTION=horovod
 #
 
 function help_me() {
-	echo "Usage: ./main.sh -n [namespace] -g [GPUNUM] -c [CPUNUM] -e [true/false] -D [NAMESPACE] -h"
+	echo "Usage: ./main.sh -n [namespace] -g [GPUNUM] -c [CPUNUM] -e [true/false] -D [NAMESPACE] -d [tensorflow/horovod]-h"
 	echo "-h		This message."
 	echo "-D [namespace]	Delete all RS in the specific NAMESPACE"
 	echo "-n [namespace]	create NAMESPACE when create RS"
 	echo "-g [GPUNUM]	trainning with GPUNUM GPUs, 0 is nothing to create"
 	echo "-g [CPUNUM]	trainning with GPUNUM CPUs, 0 is nothing to create"
 	echo "-e [true/false]	true(default) means it will create RS files and execute or without execution"
+	echo "-d [horovod/tensorflow]	choose one distribution framework to use, horovod is by default"
 }
 
 function get_opts() {
@@ -78,23 +79,32 @@ function get_opts() {
 	fi
 }
 
-function generate_rs_file() {
+function generate_horovod_rs_file() {
 	if [ $GPU_NUM -gt 0 ]; then
 		# use GPU
 		if [ $CPU_NUM -gt 0 ]; then
 			# use GPU and CPU
-			./generate_gpu_rs.py $NAMESPACE $GPU_NUM $EXEC_TRUE
-			./generate_cpu_rs.py $NAMESPACE $CPU_NUM $EXEC_TRUE
+			./generate_horovod_gpu_rs.py $NAMESPACE $GPU_NUM  $EXEC_TRUE
+			./generate_horovod_cpu_rs.py $NAMESPACE $CPU_NUM $EXEC_TRUE
 		else
 			# only GPU
-			./generate_gpu_rs.py $NAMESPACE $GPU_NUM $EXEC_TRUE
+			./generate_horovod_gpu_rs.py $NAMESPACE $GPU_NUM $EXEC_TRUE
 		fi
 	elif [ $CPU_NUM -gt 0 ]; then
 		# use CPU without GPU
-		./generate_cpu_rs.py $NAMESPACE $CPU_NUM $EXEC_TRUE
+		./generate_horovod_cpu_rs.py $NAMESPACE $CPU_NUM $EXEC_TRUE
 	else
 		# nothing to create but namespace
-		echo "nothing to do except for namespace created !!"
+		echo -e "\033[31mnothing to do except for namespace created !!\033[0m"
+	fi
+}
+
+function generate_tensorflow_rs_file() {
+	if [ $GPU_NUM -gt 0 ] && [ $CPU_NUM -gt 0 ]; then
+		# GPU only be the worker
+		./generate_tensorflow_rs.py $NAMESPACE $GPU_NUM $CPU_NUM $EXEC_TRUE
+	else
+		echo -e "\033[31mPS distribution specified 1 master(cpu) and 1 worker(gpu) at least\033[0m"
 	fi
 }
 
@@ -111,5 +121,19 @@ fi
 # create namespace
 kubectl create namespace $NAMESPACE
 
-echo "Start generate YAML RS files..."
-generate_rs_file
+
+case $DISTRIBUTION in
+	horovod)
+		echo -e "\033[36mStart generate YAML RS files...\033[0m"
+		generate_horovod_rs_file
+		;;
+	tensorflow)
+		echo -e "\033[36mStart generate YAML RS files...\033[0m"
+		generate_tensorflow_rs_file
+		;;
+	*)
+		echo -e "\033[31mWrong distribution framework support!!\033[0m"
+		help_me	
+		exit 1
+		;;
+esac
